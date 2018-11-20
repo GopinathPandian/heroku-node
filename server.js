aafe28da434cc41f8c103e1df6320bf414ec4c7d
GopinathPandian <40463691+GopinathPandian@users.noreply.github.com>
2018-11-20 14:00:38 +0530

    Added Basic Auth

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ var user = {
 };
 
 const  ProtectedRoutes = express.Router(); 
+const BasicAuthRoutes = express.Router();
 app.use('/api', ProtectedRoutes);
+app.use('/basic', BasicAuthRoutes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('Secret', secret);
@@ -72,7 +74,7 @@ ProtectedRoutes.use((req, res, next) =>{
       // if there is no token
       // return an error
       return res.status(403).send({ 
-          message: 'Access token not found!!' 
+          message: 'Bearer token not found!!' 
       });
   
     }
@@ -118,6 +120,48 @@ app.get('/getXml', (req, res)=>{
     res.set('Content-Type', 'application/xml');
     res.send(xml_res);
     res.status(200).send();
+});
+
+BasicAuthRoutes.use((req, res, next)=>{
+    const auth = {login: 'designer', password: 'designer@123'};
+    // parse login and password from headers
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
+
+    // Verify login and password are set and correct
+    if (!login || !password || login !== auth.login || password !== auth.password) {
+        res.set('WWW-Authenticate', 'Basic realm="401"') // change this
+        res.status(401).send('Authentication required.') // custom message
+        return
+    }
+    
+    // -----------------------------------------------------------------------
+    // Access granted...
+    next()
+});
+
+BasicAuthRoutes.get('/getXml', (req, res)=>{
+    console.log(JSON.stringify(req.headers));
+    console.log(JSON.stringify(req.body));
+    xml_res = '<accountLookupResults> \
+<status>success</status> \
+<variables> \
+    <variable name="IsMatch" value="N"/> \
+    <variable name="DigitalIDStatus" value="87654321:active,87654321:barred"/> \
+    <variable name="DigitalIDCount" value="2"/> \
+    <variable name="ActiveDigitalIDCount" value="1"/> \
+    <variable name="WSResult" value="success"/> \
+</variables> \
+</accountLookupResults>'
+
+    res.set('Content-Type', 'application/xml');
+    res.send(xml_res);
+    res.status(200).send();
+});
+
+BasicAuthRoutes.get('/getJson', (req, res)=>{
+    console.log(JSON.stringify(req.headers));
+    res.status(200).json({"DigitalIDStatus":"999999:active,999999:barred","DigitalIDCount": "9"});
 });
 
 http.createServer(app).listen(port);
